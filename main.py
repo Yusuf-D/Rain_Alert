e6265efe64966c9724c8a71b61b357fef58077e3
Yusuf Demirci <93779402+Yusuf-D@users.noreply.github.com>
2021-12-29 22:42:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from twilio.rest import Client
 
-account_sid = "ACa4af4229630de5bde56d8fdef36e3e7e"
-auth_token = "ffea79a6ad462e531d7110f87decdc16"
+account_sid = "YOUR_ACCOUNT_SID"
+auth_token = "YOUR_AUTH_TOKEN"
 
 parameters = {
-    "lat": 40.853271,
-    "lon": 29.881519,
-    "appid": "232401d9f0e4be3a87cca4591b428782",
+    "lat": YOUR_LATITUDE,
+    "lon": YOUR_LONGITUDE,
+    "appid": "YOUR_APP_ID",
     "exclude": "current,daily,minutely"
 }
 
@@ -25,8 +25,8 @@ if will_rain:
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to="+905541510171", 
-        from_="+12084860229",
+        to="YOUR_PHONE_NUMBER", 
+        from_="YOUR_TWILIO_ACCOUNT_PHONE_NUMBER",
         body="It's going to rain today. Remember to take an umbrella!")
 
     print(message.status)
